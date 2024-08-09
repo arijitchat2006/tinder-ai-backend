f6fdb9713b79e155cb29693ea131e962b4390fca
arijitchat2006 <arijit_chat2006@yahoo.com>
2024-08-09 16:08:53 +0530

    Add a profile to the DB and fetch it successfully.

--- src/main/java/io/techarchitect/tinder_ai_backend/TinderAiBackendApplication.java
+++ src/main/java/io/techarchitect/tinder_ai_backend/TinderAiBackendApplication.java
@@ -1,13 +1,39 @@
 package io.techarchitect.tinder_ai_backend;
 
+import io.techarchitect.tinder_ai_backend.profiles.Gender;
+import io.techarchitect.tinder_ai_backend.profiles.Profile;
+import io.techarchitect.tinder_ai_backend.profiles.ProfileRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TinderAiBackendApplication {
+public class TinderAiBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	private ProfileRepository profileRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinderAiBackendApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		Profile profile = new Profile(
+				"1",
+				"Arijit",
+				"Chatterjee",
+				"38",
+				"Indian",
+				Gender.MALE,
+				"Software Programmer",
+				"Foo.jpg",
+				"INTP"
+		);
+		profileRepository.save(profile);
+		profileRepository.findAll().forEach(System.out :: println);
+	}
 }
---
+++ src/main/java/io/techarchitect/tinder_ai_backend/profiles/Profile.java
@@ -0,0 +1,13 @@
+package io.techarchitect.tinder_ai_backend.profiles;
+
+public record Profile(
+                      String id,
+                      String firstName,
+                      String lastName,
+                      String age,
+                      String ethnicity,
+                      Gender gender,
+                      String bio,
+                      String imageUrl,
+                      String myersBriggsPersonalityType) {
+}
